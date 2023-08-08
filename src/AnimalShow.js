2f07eef8e26f949bc09f8dad6d42b1eeeecbae5b
Ankit Sharma <hustlerandlearner@gmail.com>
2023-08-08 10:49:10 +0530

    update styling

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,4 +1,22 @@
-function AnimalShow() {
-  return <div>AnimalShow</div>;
+import cat from './svg/cat.svg';
+import dog from './svg/dog.svg';
+import cow from './svg/cow.svg';
+import horse from './svg/horse.svg';
+import gator from './svg/gator.svg';
+import bird from './svg/bird.svg';
+
+const svgMap = {
+  cat,
+  dog,
+  cow,
+  horse,
+  gator,
+  bird,
+};
+
+function AnimalShow({ type }) {
+  return (
+    <img className='animalShow' style={{}} src={svgMap[type]} alt='animals' />
+  );
 }
 export default AnimalShow;
--- src/App.js
+++ src/App.js
@@ -1,4 +1,29 @@
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimals() {
+  const animals = ['cat', 'dog', 'horse', 'gator', 'cow', 'bird'];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-  return <div>App</div>;
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimals()]);
+  };
+
+  const renderedAnimals = animals.map((type, i) => {
+    return <AnimalShow type={type} key={i} />;
+  });
+  return (
+    <div>
+      <h1>Random Animal Generator</h1>
+      <button className='generateBtn' onClick={handleClick}>
+        Add Animals
+      </button>
+      <div>{renderedAnimals}</div>
+    </div>
+  );
 }
 export default App;
